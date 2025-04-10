d1a0924114d9f43a492efe7e8df145afd1b0f638
Bharat Shah <shahbharat2609@gmail.com>
2025-04-10 12:49:36 +0530

    learned query and path params

--- src/app.js
+++ src/app.js
@@ -3,13 +3,35 @@ import express from 'express'
 const app = express()
 const PORT = 7777
 
-app.get('/', (_req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
+// app.use('/', (_req, res) => {
+//     res.send('<h1>Hello World</h1>')
+// })
 
-app.get('/test', (_req, res) => {
-    res.send('<h1>Test</h1>')
-});
+
+//! Query params
+// app.get('/user', (req, res) => {
+//     console.log(req.query);
+//     res.send({
+//         firstName: "Bharat",
+//         lastName: "Shah"
+//     })
+// })
+
+//! Path params
+// app.get('/user/:userId/:password', (req, res) => {
+//     console.log(req.params);
+//     res.send({
+//         firstName: "Bharat",
+//         lastName: "Shah"
+//     })
+// })
+
+
+
+
+app.use((_req, res) => {
+    res.status(404).send("❌❌Oops! Page not found❌❌");
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT http://localhost:${PORT}`)
