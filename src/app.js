6f0f54212d5fa04aebe91b442cc323f8cdd48247
Bharat Shah <shahbharat2609@gmail.com>
2025-04-20 21:52:51 +0530

    learnt route handling using next()

--- src/app.js
+++ src/app.js
@@ -3,31 +3,14 @@ import express from 'express'
 const app = express()
 const PORT = 7777
 
-// app.use('/', (_req, res) => {
-//     res.send('<h1>Hello World</h1>')
-// })
-
-
-//! Query params
-// app.get('/user', (req, res) => {
-//     console.log(req.query);
-//     res.send({
-//         firstName: "Bharat",
-//         lastName: "Shah"
-//     })
-// })
-
-//! Path params
-// app.get('/user/:userId/:password', (req, res) => {
-//     console.log(req.params);
-//     res.send({
-//         firstName: "Bharat",
-//         lastName: "Shah"
-//     })
-// })
-
-
-
+app.use('/user', (_req, res, next) => {
+    console.log('Handling the route user');
+    // res.send('Response!!');
+    next();
+}, (_req, res) => {
+    console.log('Handling the route user 2');
+    res.send('Response 2!!');
+})
 
 app.use((_req, res) => {
     res.status(404).send("❌❌Oops! Page not found❌❌");
